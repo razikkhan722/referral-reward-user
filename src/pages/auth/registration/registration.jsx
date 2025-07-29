de810cc60560bc8c95e1d13eb6e6f86b1bbcd914
ShumailaKhan12 <shumaila1641@gmail.com>
2025-07-29 18:16:52 +0530

    Fix new bugs

--- src/pages/auth/registration/registration.jsx
+++ src/pages/auth/registration/registration.jsx
@@ -86,7 +86,7 @@ const Registration = () => {
   };
 
   return (
-    <div className="login-bg-img vh-100 overflow-hidden" id="registration-id">
+    <div className="login-bg-img overflow-hidden" id="registration-id">
       {/* Logo Section */}
       <div className="nav-logo text-center mt-0">
         <img className="header-center-img width-13" src={Logo} alt="logo" />
